064b5f40eae47cb47e2129854d4664310a7f12d8
Dayrlism-Mac <dayrl94@gmail.com>
2018-05-26 11:46:39 +0800

    ..

--- src/pages/Access/Access.js
+++ src/pages/Access/Access.js
@@ -52,56 +52,26 @@ const AccessForm = Form.create()(
       } = this.props.form;
 
       // Only show error after a field is touched.
-      const userNameError =
-        isFieldTouched("userName") && getFieldError("userName");
-      const passwordError =
-        isFieldTouched("password") && getFieldError("password");
+      const userNameError = isFieldTouched("userName") && getFieldError("userName");
+      const passwordError = isFieldTouched("password") && getFieldError("password");
 
       return (
         <Form layout="inline" onSubmit={this.handleSubmit}>
-          <FormItem
-            validateStatus={userNameError ? "error" : ""}
-            help={userNameError || ""}
-          >
+          <FormItem validateStatus={userNameError ? "error" : ""} help={userNameError || ""} >
             {getFieldDecorator("userName", {
-              rules: [
-                { required: true, message: "Please input your username!" }
-              ]
+              rules: [ { required: true, message: "Please input your username!" } ]
             })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Username"
-              />
+              <Input prefix={ <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} /> } placeholder="Username" />
             )}
           </FormItem>
-          <FormItem
-            validateStatus={passwordError ? "error" : ""}
-            help={passwordError || ""}
-          >
+          <FormItem validateStatus={passwordError ? "error" : ""} help={passwordError || ""} >
             {getFieldDecorator("password", {
-              rules: [
-                { required: true, message: "Please input your Password!" }
-              ]
+              rules: [ { required: true, message: "Please input your Password!" } ]
             })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="Password"
-              />
+              <Input prefix={ <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} /> } type="password" placeholder="Password" />
             )}
           </FormItem>
-          <FormItem>
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={hasErrors(getFieldsError())}
-            >
-              Log in
-            </Button>
+          <FormItem> <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())} > Log in </Button>
           </FormItem>
         </Form>
       );
