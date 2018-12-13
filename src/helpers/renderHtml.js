c3ed1ea8b66cc845bc70272342b34aa83aabfbc6
Dayrlism-Mac <dayrl94@gmail.com>
2018-12-13 09:17:27 +0800

    ..

--- src/helpers/renderHtml.js
+++ src/helpers/renderHtml.js
@@ -17,6 +17,15 @@ export default (
     <!doctype html>
     <html ${head.htmlAttributes.toString()}>
       <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-65313287-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-65313287-2');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!--[if IE]>
